d1f898c16e2e2d66289b552ed053f5de63d04548
AnggaOffc <anggastoregg2005@gmail.com>
2024-08-19 22:38:07 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
-//base by DGXeon
+//base By Angga Offc
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+//YouTube: @bacotamatpro03
+//Instagram: angga01store
+//Telegram: t.me/CobaDuluBaru
+//GitHub: @Thomzh
+//WhatsApp: +62895411941384
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@MODSDevoloper
 
 const fs = require('fs')
 const chalk = require('chalk')
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.ytname = "YT: AnggaOfficial" //ur yt chanel name
+global.socialm = "GitHub: Thomzh" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = 'Chems Bot MD V11' //ur bot name
+global.ownernumber = '62895411941384' //ur owner number
+global.ownername = '🦄드가이 AnggaOffc' //ur owner name
+global.websitex = "https://youtu.be/mu5m6aB6P6k"
+global.wagc = "https://whatsapp.com/channel/0029VaaEwLG72WU0Auv7JO0t"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "AnggaOffc Bot Inc."
+global.botscript = 'https://github.com/Thomzh/CheemsBot-MD11' //script link
+global.packname = "Sticker By AnggaOffc"
+global.author = "🦄림 가 AnggOffc\n\n+916909137213"
+global.creator = "62895411941384@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["62895411941384"] // Premium User
 global.hituet = 0
 
 //bot sett
